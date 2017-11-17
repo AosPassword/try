d0fb9f94e5b3d6345576b91aa135235ff7e6ca4f
Password <815923529@qq.com>
2017-11-17 13:14:08 +0800

    Initial commit

---
+++ src/Try.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Try{
+    public static void main(String[] args) {
+        HashSet collection=new HashSet();
+        collection.add("18223829735");
+        collection.add("138839121934");
+        collection.add("17783101935");
+        collection.add("13738401562");
+        collection.add("15826003846");
+        collection.add("17723293834");
+        collection.add("17783101935");
+        collection.add("18223829735");
+        System.out.println(collection);
+    }
+}
